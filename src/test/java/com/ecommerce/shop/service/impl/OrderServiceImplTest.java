c4dde20fde0a0af8cd43a435664e1c2e2ee9953e
kiazi <kiazim70@gmail.com>
2023-08-02 21:43:57 +0200

    commit lot file

---
+++ src/test/java/com/ecommerce/shop/service/impl/OrderServiceImplTest.java
@@ -0,0 +1,18 @@
+package com.ecommerce.shop.service.impl;
+
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created By Joshua Stamps on 2/27/2021.
+ */
+public class OrderServiceImplTest {
+
+    @Test
+    public void finish() {
+    }
+
+    @Test
+    public void cancel() {
+    }
+}
